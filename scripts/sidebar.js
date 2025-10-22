da29e0ed3de4b8378f70537d34846b6d3c245a34
Cristina <cparasciuc1@gmail.com>
2025-10-22 11:32:46 +0300

    Changed the code in sidebar.js

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,10 +1,21 @@
-// Load sidebar menu dynamically from JSON
-fetch('./data/menu.json')
-    .then(Response => Response.json())
-    .then(menu =>{
-        const sidebar=document.querySelector('.sidebar';
-            if(!sidebar) return;
+async function loadSidebar() {
+    try{
+        const response = await fetch("./data/sidebar.html");
+        const menu = await response.json();
 
+        const sidebar = document.createElement("aside");
+        sidebar.className="sidebar";
 
-        )
-    })
+        let html=`
+        <div class="sidebar-header">
+            <div class="logo">
+                <p>Connexa</p>
+                <button id="toggle-btn" title="Toggle Menu">
+                    img src="./icons-menu/arrows.png" alt="Toggle Menu Icon"
+                </button>
+            </div>
+
+        `;
+    } 
+    
+}
