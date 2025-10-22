97e80fdfead303a80da31fde0f63bf92b222e32d
Cristina <cparasciuc1@gmail.com>
2025-10-22 10:30:29 +0300

    Setup JSON-powered sidebar with global and page CSS

---
+++ scripts/sidebar.js
@@ -0,0 +1,10 @@
+// Load sidebar menu dynamically from JSON
+fetch('./data/menu.json')
+    .then(Response => Response.json())
+    .then(menu =>{
+        const sidebar=document.querySelector('.sidebar';
+            if(!sidebar) return;
+
+
+        )
+    })
